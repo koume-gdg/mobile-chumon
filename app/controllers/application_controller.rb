db72b8cf934871a4ae5b2f9b5a88c13c18b9e91d
koume-gdg <gdgpll.vmj@gmail.com>
2021-02-26 16:58:00 +0900

    新規登録機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:shop_name, :owner_name, :owner_phone_number, :shop_address, :shop_city, :email])
+  end
 end
