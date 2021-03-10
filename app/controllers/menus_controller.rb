8eb8373fc538690b7b03ff7f07e2ce2e8e8bbc8a
koume-gdg <gdgpll.vmj@gmail.com>
2021-03-10 23:50:02 +0900

    controller記述

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,5 +1,24 @@
 class MenusController < ApplicationController
   def index
   end
-  
+
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+    if @menu.save
+      redirect_to munu_path
+    else
+      render :new
+    end
+
+  end
+
+  private
+  def menu_params
+    params.require(:menu).permit(:name, :price).merge(user_id: current_user.id)
+  end
+
 end
